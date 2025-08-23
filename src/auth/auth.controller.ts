48c8d6ecf37149753f5c8930eb3e421571119869
Pe-te-r <mburupeter8526@gmail.com>
2025-08-23 12:34:29 +0300

    schema

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
 import type { Context } from "hono";
 
 export const registerController = async(c:Context)=>{
-
+    const registerData = await c.req.json();
+    
 }
 
 export const loginController = async(c:Context)=>{
