1f53aed5a08f4b1484de69267a8600b0b7be1aec
Pe-te-r <mburupeter8526@gmail.com>
2025-08-24 02:18:54 +0300

    auth configs

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
     dialect: "postgresql",   
-    schema: "./src/drizzle/schema.ts",
-    out: "./src/drizzle/migrations",
+    schema: "./src/db/schema.ts",
+    out: "./src/db/migrations",
     dbCredentials: {
         url: process.env.Database_URL as string,
     },
